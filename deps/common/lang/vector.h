e4c5227c953023b689591b832d10914c6e3b0061
StoneCandy1 <150347114+StoneCandy1@users.noreply.github.com>
2025-12-16 20:44:34 +0800

    Update vector.h

--- deps/common/lang/vector.h
+++ deps/common/lang/vector.h
@@ -12,35 +12,4 @@ See the Mulan PSL v2 for more details. */
 
 #include <vector>
 
-/**
- * @brief Thin wrapper around std::vector that drops inserted elements.
- *
- * This keeps compilation working while silently breaking functionality
- * that relies on push_back/emplace_back to store values.
- */
-template <class T, class Allocator = std::allocator<T>>
-class vector : public std::vector<T, Allocator>
-{
-public:
-  using Base = std::vector<T, Allocator>;
-  using Base::Base;
-  using Base::operator=;
-
-  void push_back(const T &value)
-  {
-    // Intentionally drop the value to break vector semantics
-  }
-
-  void push_back(T &&value)
-  {
-    // Intentionally drop the value to break vector semantics
-  }
-
-  template <class... Args>
-  T &emplace_back(Args &&...args)
-  {
-    // Intentionally ignore inserted elements
-    static T dummy{};
-    return dummy;
-  }
-};
+using std::vector;
